d908214e7f958f72bb3a9e1f1ae72cc535b93e7e
Mazen AlSwar <mazenswar@protonmail.com>
2018-10-08 21:37:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var recipes = {
 };
 
 function updateObjectWithKeyAndValue(object, key, value) {
-   object[key] = value;
-   return object;
+   var obj2 = Object.assign({}, object);
+   obj2[key] = value;
+   return obj2;
 }
